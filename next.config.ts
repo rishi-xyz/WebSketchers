87f78368dab0a336612ea0fa995444458433e938
rishi-xyz <itsranahrishikesh@gmail.com>
2024-12-17 22:15:19 +0530

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images:{
+    domains:[
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ]
+  },
+  reactStrictMode:false,
+};
+
+export default nextConfig;
