6b32000daf7e1985301d7a0a8cb37a7d71d535f1
rishi-xyz <itsranahrishikesh@gmail.com>
2024-12-21 20:02:02 +0530

    modified Marketing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { Navbar } from "@/components/Navbar";
-
+//374151
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="h-full w-full relative flex items-center justify-center flex-col bg-black/90">
+      <section className="h-full w-full relative flex items-center justify-center flex-col bg-[radial-gradient(ellipse_250%_100%_at_center_right,#0066FF,#000000_20%)]">
         {/*Grids */}
         <div className=" absolute h-full w-full">
-          <div className=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,rgba(255,255,255,0.5)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.3)_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+          <div className=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,rgba(255,255,255,0.5)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.3)_1px,transparent_1px)] bg-[size:44px_54px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
         </div>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative -top-16">
           <h1 className="text-9xl font-bold text-center md:text-[200px]">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 
 export const Navbar = () => {
   return (
-    <header className="sticky z-40 top-0 bg-transparent backdrop-blur-sm">
+    <header className="fixed w-full z-40 top-0 bg-transparent backdrop-blur-sm">
       <div className="container">
         <div className="flex justify-between items-center h-20">
           <h2 className="text-3xl md:text-4xl md:leading-none font-semibold tracking-tighter text-center ml-1 md:ml-5 bg-primary/75 text-transparent bg-clip-text">
