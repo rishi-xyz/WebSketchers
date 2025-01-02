212422db2e340c51b8b01816357d79bdaa00506d
rishi-xyz <itsranahrishikesh@gmail.com>
2025-01-02 16:00:42 +0530

    Added form components

--- app/(platform)/agency/page.tsx
+++ app/(platform)/agency/page.tsx
@@ -43,7 +43,7 @@ const AgencyPage = async ({
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl">Create An Agency</h1>
+        <h1 className="text-4xl m-3 font-bold">Create An Agency</h1>
         <AgencyDetails
           data={{companyEmail:authUser?.emailAddresses[0].emailAddress}}
         />
