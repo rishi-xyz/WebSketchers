9a9315eb8fe26e3534185f18b0fd81baa8bb514b
rishi-xyz <itsranahrishikesh@gmail.com>
2025-01-03 11:50:45 +0530

    Added domain page

--- app/[domain]/[path]/page.tsx
+++ app/[domain]/[path]/page.tsx
@@ -2,25 +2,29 @@ import FunnelEditor from "@/app/(platform)/subaccount/[subaccountId]/funnels/[fu
 import { getDomainContent } from "@/lib/queries";
 import EditorProvider from "@/src/providers/editors/editors-providers";
 import { notFound } from "next/navigation";
+import React from "react";
 
-const PathPage = async ({params}:{params:{domain:string;path:string}})=>{
-    const domainData = await getDomainContent(params.domain.slice(0,-1));
-    const pageData = domainData?.FunnelPages.find((page)=>page.pathName === params.path);
-    if(!pageData||!domainData){
-        return notFound();
-    }
-    return(
-        <EditorProvider
-        subaccountId={domainData.subAccountId}
-        pageDetails={pageData}
-        funnelId={domainData.id}
-        >
-            <FunnelEditor
-            funnelPageId={pageData.id}
-            liveMode={true}
-            />
-        </EditorProvider>
-    );
-}
+const Page = async ({
+  params,
+}: {
+  params: { domain: string; path: string };
+}) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1));
+  const pageData = domainData?.FunnelPages.find(
+    (page) => page.pathName === params.path
+  );
 
-export default PathPage;
+  if (!pageData || !domainData) return notFound();
+
+  return (
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={domainData.id}
+    >
+      <FunnelEditor funnelPageId={pageData.id} liveMode={true} />
+    </EditorProvider>
+  );
+};
+
+export default Page;
